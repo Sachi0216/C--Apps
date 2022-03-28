51fb80cd80fc2ee46c9692ab377eaa1391ec0c5b
Sachi0216 <davis.elijah15@hotmail.com>
2022-03-28 11:12:33 -0500

    Add project files.

---
+++ Anonymous Income Comparison/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Anonymous_Income_Comparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.ReadLine();
+
+            // person 1 information
+            Console.WriteLine("Person 1 Salary information");
+            Console.ReadLine();
+
+            Console.WriteLine("What is your hourly rate?");
+            int hourlyRate = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hours worked per week?");
+            int weeklyHours = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Annual Salary of Person 1:");
+            int product = hourlyRate * weeklyHours * 52;
+            Console.WriteLine(product);
+
+            // Person 2 information
+
+            Console.WriteLine("Person 2");
+            Console.ReadLine();
+
+            Console.WriteLine("Hourly pay?");
+            int p2hourlyRate = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hours worked per week?");
+            int p2weeklyHours = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Annual Salary of Person 2:");
+            int salary = p2hourlyRate * p2weeklyHours * 52;
+            Console.WriteLine(salary);
+
+            // Comaparison
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            Console.ReadLine();
+
+            bool trueOrFalse = product > salary;
+            Console.WriteLine(trueOrFalse.ToString());
+            Console.ReadLine();
+
+
+        }
+    }
+}
