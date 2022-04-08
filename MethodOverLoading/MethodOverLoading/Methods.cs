606723c65be63fcb69cabac3792bfadd0a730dae
Sachi0216 <davis.elijah15@hotmail.com>
2022-04-07 19:34:59 -0500

    int

---
+++ MethodOverLoading/MethodOverLoading/Methods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    public class Methods
+    {
+        public int Add(int Value)
+        {
+
+            return Value + 4;
+        }
+
+        public int Add(decimal Value)
+        {
+            return Convert.ToInt32(Value - 0.2m);
+        }
+        // type infront of method that determines type returned
+        public int Add(string data)
+        {
+            // Converting string to int (declare type of integer before variable)
+            // keyword int infront of the variable declares what it is// // type which is integer
+            int newInt = Convert.ToInt32(data);
+
+            return Convert.ToInt32(newInt * 3);
+
+        }
+    } // all methods returning an integer
+}
---
+++ MethodOverLoading/MethodOverLoading/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        public static int Data { get; set; }
+        static void Main(string[] args)
+        {// main
+            Methods methods = new Methods();
+            Methods methods2 = new Methods();
+            Methods methods3 = new Methods();
+
+
+
+
+            int result = methods.Add(2);
+            Console.WriteLine(result);
+
+            result = methods2.Add(2.2m);
+            Console.WriteLine(result);
+            //int result3 = methods.Divide(result, 1);
+
+            result = methods3.Add("2");
+            Console.WriteLine(result);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
+// methods have to be part of a class
+
