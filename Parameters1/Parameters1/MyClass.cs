c8afae6318fceb7bb939bdead5939c437fec11c8
Sachi0216 <davis.elijah15@hotmail.com>
2022-04-08 17:06:32 -0500

    int

---
+++ Parameters1/Parameters1/MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parameters1
+{
+    public class MyClass
+    {
+        // default value is 2(will pass in with 
+        public int Add(int data, int value = 2)
+        {
+            // set optional value
+            
+            return data + value;
+        } 
+    }
+}
---
+++ Parameters1/Parameters1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Parameters1
+{
+    class Program
+    {
+        public static int Data { get; set; }
+        static void Main(string[] args)
+        {
+               
+            Console.WriteLine("Please input two numbers\n Input numbber one");
+            int number = Convert.ToInt32(Console.ReadLine());
+            // creating var for user input and converting to int
+            Console.WriteLine("Input second number: This one is optional");
+            // Keyword optional(create if/else to verify if second number was given)
+            string numberStr = Console.ReadLine();
+
+            MyClass myclass = new MyClass();
+           // taking string var and checking if its empty, if so no var was entered and will pass default value set in class
+           // setting numberStr to empty str with ""
+            if (numberStr == "")
+            {
+              
+                int i = myclass.Add(number);
+                Console.WriteLine(i);
+            }
+            else
+            {
+                int j = Convert.ToInt32(numberStr);
+                int result = myclass.Add(number, j);
+                Console.WriteLine(result);
+            }
+                // if they didnt enter a second number 
+                // Call method with one paremeter
+                // else call method with two parameters
+
+
+         
+            Console.ReadLine();
+
+        }
+        
+    }
+    
+}
