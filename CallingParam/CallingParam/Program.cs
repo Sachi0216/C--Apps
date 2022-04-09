b0b49097b0e1c4c8da2fad3b1bd521bf3f7727e8
Sachi0216 <davis.elijah15@hotmail.com>
2022-04-08 19:36:07 -0500

    int

---
+++ CallingParam/CallingParam/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CallingParam
+{
+    class Program
+    {
+        public static int Data { get; set; }
+        static void Main(string[] args)
+        {
+            // instantiate class
+            SubAssign subAssign = new SubAssign();
+
+            // call method in the class passing two ints
+
+            int i = 3;
+            int j = 4;
+            subAssign.Intap(i, j);
+
+            subAssign.Intap(num1: 2, num2: 4);
+
+            Console.ReadLine();
+        }
+    }
+    
+}
---
+++ CallingParam/CallingParam/SubAssign.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallingParam
+{
+    class SubAssign
+    {
+        // method that takes in two integers as param
+        // using void method(no return )
+        public void Intap( int num1, int num2)
+        {
+            // math op on first parameter
+            int result = num1 + 5;
+            num2 = 3;
+            Console.WriteLine(num2);
+        }
+    }
+}
