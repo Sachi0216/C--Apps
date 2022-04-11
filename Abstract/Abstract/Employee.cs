14db0c63379394acdb09a0c8cd42c982808c818f
Sachi0216 <davis.elijah15@hotmail.com>
2022-04-11 16:51:33 -0500

    int

---
+++ Abstract/Abstract/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstract
+{
+    // inheriting from person class
+    public class Employee : Person
+    {
+        public override void SayName()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+        
+    }
+}
---
+++ Abstract/Abstract/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instatiate/ obj
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.SayName();
+            Console.ReadLine();
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
