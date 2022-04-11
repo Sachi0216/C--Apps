a42da021cb0d0e1091340239ccd240179c7258ef
Sachi0216 <davis.elijah15@hotmail.com>
2022-04-11 18:11:25 -0500

    int

---
+++ Operators/Operators/Employees.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operators
+{
+    public class Employees
+    {
+        // overloading operator to see if two employee objects are equal
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+        // if equal to == 
+        public static bool operator ==(Employees employee1, Employees employee2)
+        {
+            // checking by employee id
+            return employee1.Id == employee2.Id;
+        }
+        // returning false !=
+        public static bool operator !=(Employees employee1, Employees employee2)
+        {
+            return employee1.Id != employee2.Id;
+
+        }
+    }
+}
---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instatiate
+            Employees employee1 = new Employees();
+            Employees employee2 = new Employees();
+            // giving same first and last name since method called for employee id however
+            
+            employee1.Firstname = "Eli";
+            employee1.Lastname = "Davis";
+            employee2.Firstname = "Eli";
+            employee2.Lastname = "Davis";
+            // what i'm checking for, equal or not equal employee id
+            employee1.Id = 44;
+            employee2.Id = 42;
+            Console.WriteLine("Does employee1 and employee2 have the same first names? " + (employee1.Firstname == employee2.Firstname));
+            Console.WriteLine("Does employee1 and employee2 have same last names? " + (employee1.Lastname == employee2.Lastname));
+            // overloaded == operator to take in two employee obj's and returned if thoe are equal by comparing id's inside the method. 
+            Console.WriteLine((employee1 == employee2));
+        }
+    }
+}
