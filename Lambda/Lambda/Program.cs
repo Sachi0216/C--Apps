816c1b58fa9ba3bc816e6f5c0658321ba98e0305
Sachi0216 <davis.elijah15@hotmail.com>
2022-04-12 14:40:25 -0500

    ini

---
+++ Lambda/Lambda/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List of at least 10 employees
+            List<string> Employees = new List<string>(); // { "James Doe", "Martin Lawrence", "Sam Jones", "Rad Richy", "Max Pane", "Charles Barkley", "Harry Potter", "Thomas Jefferson", "Joe Mo", "Joe Tang" };
+            
+            
+            Employees.Add("James Doe");
+            Employees.Add("Martin Lawrence");
+            Employees.Add("Sam Jones");
+            Employees.Add("Rad Richy");
+            Employees.Add("Max Pane");
+            Employees.Add("Charles Barkley");
+            Employees.Add("Harry Potter");
+            Employees.Add("Thomas Jefferson");
+            Employees.Add("Joe Mo");
+            Employees.Add("Joe Tang");
+
+
+            //Foreach loop
+
+            foreach (string employees in Employees)
+            {
+                Console.WriteLine(employees);
+                Console.WriteLine(employees.Contains("Joe"));
+                Console.ReadLine();
+
+            }
+
+        }
+    }
+}
---
+++ Lambda/Lambda/TheLambda.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lambda
+{
+    public class TheLambda
+    {
+        public string Employees { get; set; }
+        public string ID { get; set; }
+    }
+}
