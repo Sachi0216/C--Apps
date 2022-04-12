824fd7d80305af1d4b27ea02f9c8f5cabb70b451
Sachi0216 <davis.elijah15@hotmail.com>
2022-04-11 20:24:06 -0500

    int

---
+++ Enum(DaysoftheWeek)/Enum(DaysoftheWeek)/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_DaysoftheWeek_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an enum for the days of the week
+
+            // Prompt the user to enter the current day of the week.
+
+            // Assign the value to a variable of that enum data type you just created.
+
+            //Wrap the above statement in a try/catch block and have it print "Please enter an actual day of the week.” to the console if an error occurs.
+            // Try/Catch 
+            try
+            {
+                TheDaysOfTheWeek theDays = new TheDaysOfTheWeek();
+                Console.WriteLine("Please select the current day of the week?");
+
+                string daySelected = Console.ReadLine();
+
+                var TheDay = System.Enum.Parse(typeof(TheDaysOfTheWeek), daySelected);
+
+                TheDaysOfTheWeek parsedEnumValue = (TheDaysOfTheWeek)System.Enum.Parse(typeof(TheDaysOfTheWeek), daySelected);
+
+                //Console.WriteLine(System.Enum.Parse(typeof(Enum_DaysoftheWeek_.Days), daySelected));
+
+                //Console.WriteLine("Today is : ", (TheDaysOfTheWeek)System.Enum.Parse(typeof(TheDaysOfTheWeek), daySelected));
+                Console.WriteLine("Today is {0}. ", (TheDaysOfTheWeek)System.Enum.Parse(typeof(TheDaysOfTheWeek), daySelected));
+
+                //Console.WriteLine(System.Enum.GetName(typeof(Enum_DaysoftheWeek_.Days), theDays));
+
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Unable to convert, Please enter an actual day of the week. ");
+                return;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+
+        }
+
+    }
+}
---
+++ Enum(DaysoftheWeek)/Enum(DaysoftheWeek)/TheDaysOfTheWeek.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_DaysoftheWeek_
+{
+    public class TheDaysOfTheWeek
+    {
+        public Days TheDay { get; set; }
+    }
+        public enum Days
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Satuday
+        }
+
+
+
+
+}
