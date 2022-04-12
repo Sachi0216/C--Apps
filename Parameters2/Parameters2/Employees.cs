9b93c0bb46f3f2482e5c48b0e0791a0fa99ecc0b
Sachi0216 <davis.elijah15@hotmail.com>
2022-04-11 19:33:42 -0500

    int

---
+++ Parameters2/Parameters2/Employees.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parameters2
+{
+    public class Employees<T>
+    {
+        public List<T> Things { get; set; }
+    }
+}
---
+++ Parameters2/Parameters2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instatiate with string type
+            Employees<string> things1 = new Employees<string>();
+            // List of strings
+            things1.Things = new List<string>() { "Jedi", "Mandalorian", "Sith" };
+            // int instatiation
+            Employees<int> things2 = new Employees<int>();
+            // List of intehers
+            things2.Things = new List<int>() { 1, 2, 3, 4 };
+
+            // loop that prints all of the "things" 
+            foreach (string items1 in things1.Things)
+            {
+                Console.WriteLine(items1);
+            }
+
+            foreach (int items2 in things2.Things)
+            {
+                Console.WriteLine(items2);
+            }
+            Console.ReadLine();
+        }
+    }
+}
