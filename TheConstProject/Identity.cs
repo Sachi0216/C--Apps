3edcd8155f516542bcb9b3fac30673e1c05b6ab9
Sachi0216 <davis.elijah15@hotmail.com>
2022-04-14 14:37:12 -0500

    initial

---
+++ TheConstProject/Identity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheConstProject
+{
+    public class Identity
+    {
+        public string name;
+        public int id;
+
+        public Identity(string studentName, int Id)
+        {
+
+        }
+        public Identity(string name) : this(name, 1234)
+        {
+
+            this.name = name;
+
+        }
+        public Identity(int id) : this("R2D2", id)
+        {
+            this.id = id;
+        }
+
+        public Identity()
+        {
+        }
+    }
+}
---
+++ TheConstProject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TheConstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Create a const variable
+            //2.Create a variable using the keyword "var".
+            //3.Chain two constructors together.
+
+            const string myIdentity = "Elijah Davis";
+            Console.WriteLine("Hello, {0}.", myIdentity);
+            var identity = new Identity();
+            identity.id = 105579;
+            identity.name = "Eli";
+            Console.WriteLine(identity.id);
+            Console.WriteLine(identity.name);
+            Console.ReadLine();
+        }
+    }
+}
